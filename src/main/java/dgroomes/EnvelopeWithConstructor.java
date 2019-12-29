c947e54e47de7c807b2fb43eef1c3025df37d607
davidgroomes <dwgroomes@gmail.com>
2019-12-28 19:13:55 -0600

    Clarify examples with comments and links to Jackson docs

--- src/main/java/dgroomes/EnvelopeWithConstructor.java
+++ src/main/java/dgroomes/EnvelopeWithConstructor.java
@@ -4,6 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Objects;
 
+/**
+ * Ideally, there should be no Jackson annotations in this class if we aim to keep our model classes decoupled from the
+ * deserialization/serialization concerns by leveraging the Jackson ParameterNamesModule. But there is a special case
+ * for classes that have a single-arg constructor where @JsonProperty("propertyName") is required. This class
+ * illustrates that issue.
+ *
+ * Also, see the note at See note at https://github.com/FasterXML/jackson-modules-java8/pull/26/files
+ */
 public class EnvelopeWithConstructor implements Envelope {
 
     private String message;
--- src/main/java/dgroomes/EnvelopeWithConstructorWithExtraneousField.java
+++ src/main/java/dgroomes/EnvelopeWithConstructorWithExtraneousField.java
@@ -2,6 +2,10 @@ package dgroomes;
 
 import java.util.Objects;
 
+/**
+ * Companion example to dgroomes.EnvelopeWithConstructor which illustrates how a model class be completely bare of
+ * Jackson annotations.
+ */
 public class EnvelopeWithConstructorWithExtraneousField implements Envelope {
 
     private String message;
